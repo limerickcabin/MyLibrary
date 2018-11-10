6bfadb81fe7ee106f05341ace5b263c08969ce19
David DeLong <limerickcabin@hotmail.com>
2018-11-10 11:05:41 -0800

    Add files via upload

---
+++ src/DelongTime.h
@@ -0,0 +1,18 @@
+#define TIMEZONE (-7)
+
+// ascii time stamp tied to NTP
+char timebuf[80];
+char * gettime(void) {
+  time_t snap;
+  time(&snap);
+  strcpy(timebuf,ctime(&snap));
+  //*strstr(timebuf,"\n")=0;
+  strncpy(timebuf, timebuf+11, 8);
+  timebuf[5]=0;
+  return timebuf;
+}
+
+void setupTime(void) {
+  configTime(TIMEZONE*3600, 0, "pool.ntp.org");
+}
+
