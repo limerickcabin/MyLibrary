07b7d8985072c83e7189562d2d2fe28aae0cd31b
David DeLong <limerickcabin@hotmail.com>
2018-11-10 16:58:12 -0800

    Update DelongTime.h

--- src/DelongTime.h
+++ src/DelongTime.h
@@ -1,21 +1,19 @@
 #ifndef delongtime_h
 #define delongtime_h
 
-#define TIMEZONE (-8)
-
 // ascii time stamp tied to NTP
-char timebuf[80];
+char _timebuf[80];
 char * gettime(void) {
   time_t snap;
   time(&snap);
-  strcpy(timebuf,ctime(&snap));
+  strcpy(_timebuf,ctime(&snap));
   //*strstr(timebuf,"\n")=0;
-  strncpy(timebuf, timebuf+11, 8);
-  timebuf[8]=0;
-  return timebuf;
+  strncpy(_timebuf, _timebuf+11, 8);
+  _timebuf[8]=0;
+  return _timebuf;
 }
 
-void setupTime(void) {
-  configTime(TIMEZONE*3600, 0, "pool.ntp.org");
+void setupTime(int timezone) {
+  configTime(timezone*3600, 0, "pool.ntp.org");
 }
 #endif
